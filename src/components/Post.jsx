777a2a8d5c33192f586eb6086186aa4fd43e0d28
Henrique <henbarros694@gmail.com>
2025-01-16 13:32:23 -0300

    regras e mensagens de erro antes que o usuário faça o submit do formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,9 +33,14 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText('');
     }
 
-    function handleNewCommentChange(event) {
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
 
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
     function deleteComment(commentToDelete) {
@@ -45,7 +50,9 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeleteOne);
     }
-    
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
     return (
         <article className={styles.post}>
             <header>
@@ -85,10 +92,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
