8239aef3fb36d152a455e74aa78c5a0a84f1e55a
Henrique <henbarros694@gmail.com>
2025-01-16 16:12:15 -0300

    Adição do evento do like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,13 +2,19 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+
+    const [likeCount, setLikeCount] = useState(0);
     
     function handleDeleteComment() {
-
         onDeleteComment(content);
     } 
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
     
     return (
         <div className={styles.comment}>
@@ -32,8 +38,8 @@ export function Comment({ content, onDeleteComment }) {
 
                 <footer>
                     <button>
-                        <ThumbsUp size={20} />
-                        Aplaudir <span>20</span>
+                        <ThumbsUp onClick={handleLikeComment} size={20} />
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
